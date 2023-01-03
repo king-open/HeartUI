8f6d4c9ff559e18da306e50283624e9c4c0e9bfb
king-open <kingwang.0624@gmail.com>
2023-01-03 16:18:55 +0800

    优化 css 代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -24,36 +24,27 @@ export default {
 $h: 22px;
 $h2: $h - 4px;
 button {
-  height: $h;
-  width: $h * 2;
-  border: none;
-  background: #bfbfbf;
-  border-radius: $h/2;
-  position: relative;
+  height: $h;width: $h * 2;border: none;
+  background: #bfbfbf;border-radius: $h/2;position: relative;
+  > span {
+  position: absolute;top: 2px;left: 2px;height: $h2;
+  width: $h2;background: white; border-radius: $h2 / 2;transition: all 250ms;
 }
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
-  transition: all 250ms;
-}
-button.checked{
-    background: #87cefa;
-  }
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
-button:focus{
-  outline: none;
-}
-button:active{
+&.checked{ background: #87cefa;
+    > span {left: calc(100% - #{$h2} - 2px);}
+&:focus{outline: none;}
+&:active{
     > span {width: $h2 + 4px;}
   }
-  button.checked:active{
+  &.checked:active{
     > span {width: $h2 + 4px; margin-left: -4px;}
   }
+  }
+}
+
+
+
+
+
+  
 </style>
