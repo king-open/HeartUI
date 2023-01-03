380b3c845f1043cbed8085f063ac04ec41d9c3cf
king-open <kingwang.0624@gmail.com>
2023-01-03 16:02:49 +0800

    使用 v-model

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -8,11 +8,11 @@
 import { ref } from "vue";
 export default {
   props:{
-    value:Boolean
+    value:Boolean,
   },
   setup(props,context){
     const toggle = ()=>{
-      context.emit('input',!props.value)
+      context.emit('update:value',!props.value)
     }
     return {toggle}
   } 
