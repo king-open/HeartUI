4a52c2949571f89d60ae14efefd95fb36bbb9408
king-open <kingwang.0624@gmail.com>
2023-01-03 14:20:25 +0800

    搭建html 结构

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    1
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>0.2℃</h1>
+      <h2>一个很厉害的UI 框架</h2>
+      <p class="actions">
+        <a href="">Github</a>
+        <a href="">Start</a>
+      </p>
+    </div>
   </div>
 </template>
 
