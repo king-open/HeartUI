382b4cdc4f72dadc2328d8648dd1ef51784f487f
king-open <kingwang.0624@gmail.com>
2023-01-04 14:07:10 +0800

    添加 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,19 +1,17 @@
 <template>
-  <div :size="size">
-  <button v-bind="rest">
+  <button class="love-button" :class="{[`theme-${theme}`]:theme}">
     <slot/>
   </button>
-  </div>
 </template>
 
 <script lang="ts">
 export default{
-  inheritAttrs:false,
-  props:{},
-  setup(props,context){
-    const {size, ...rest} = context.attrs;
-    return {size,rest};
-  }
+  props:{
+    theme:{
+      type:String,
+      default:'button'
+    },
+  },
 }
 </script>
 
