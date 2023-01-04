6de13a13d5be3e2737d6cbf12e92934ea8ce6c92
king-open <kingwang.0624@gmail.com>
2023-01-04 13:53:07 +0800

    创建 Button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,15 +1,23 @@
 <template>
+  <div>Button 示例</div>
+  <h1>示例 1</h1>
   <div>
-    Button 示例
+    <Button>你好</Button>
   </div>
 </template>
 
-<script>
-  export default {
-    
-  }
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    return { onClick };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
