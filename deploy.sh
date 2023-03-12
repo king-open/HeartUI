28713f2f5450ff9137f347fd9064bc465e1aeaef
king-open <kingwang.0624@gmail.com>
2023-03-12 15:26:40 +0800

    😄

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+npm run build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M main &&
+git remote add origin git@github.com:king-open/limitUI.git &&
+git push -f -u origin main &&
+cd -
+
